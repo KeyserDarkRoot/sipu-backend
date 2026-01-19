249637faa768099d415a1cce181d8890abe36a2a
KeyserDarkRoot <broshmario44@gmail.com>
2026-01-19 05:29:00 -0500

    sss

---
+++ app/database/ConexionBD/config_DB.py
@@ -0,0 +1,7 @@
+import os
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise Exception("Faltan variables de entorno SUPABASE")
